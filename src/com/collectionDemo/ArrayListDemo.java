f3fa81e51261d12acd8a73ef57acf7cf2540d7ed
Dasilva20 <dasilvatobi12@gmail.com>
2023-09-14 15:06:53 +0100

    recovery

--- src/com/collectionDemo/ArrayListDemo.java
+++ src/com/collectionDemo/ArrayListDemo.java
@@ -3,7 +3,8 @@ package com.collectionDemo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ArrayListDemo {
+public class ArrayListDemo
+{
     public static void main(String[] args){
         List list = new ArrayList<>();
 
